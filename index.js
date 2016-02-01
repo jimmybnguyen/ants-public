59f5d05dcfc199fd469fe90b1b6f3b336ac2b3f0
jimmybnguyen <me@jimmybnguyen.com>
2016-02-01 00:28:07 -0800

    Add queen double damage

--- index.js
+++ index.js
@@ -4,17 +4,12 @@ var ants = require('./lib/ants_ui.js'); //load the game (UI-level) to interact w
 
 if(process.argv[2] === '--debug'){
   //the scenario to play
-  var colony = ants.AntColony.createDefaultColony();
+  var colony = ants.AntColony.createTestColony();
   var hive = ants.Hive.createTestHive();
   var game = new ants.AntGame(colony, hive);
   
-  game.deployAnt('Bodyguard', '0,5');
-  game.takeTurn();
-  game.takeTurn();
-  game.takeTurn(); 
-  game.takeTurn(); 
-  game.takeTurn(); 
-  game.deployAnt('Fire', '0,4');
+  game.deployAnt('Queen', '0,1');
+  game.deployAnt('Fire', '0,2');
   game.takeTurn();
   game.takeTurn(); 
   game.takeTurn(); 
